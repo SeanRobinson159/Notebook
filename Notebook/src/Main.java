c6a74cecdf4f9fa4ba22d1d746938cd55337ddb0
SeanRobinson159 <SeanRobinson159@gmail.com>
2014-10-27 10:25:31 -0600

    Added Main class

---
+++ Notebook/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sean Robinson
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
